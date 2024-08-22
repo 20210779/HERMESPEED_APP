0e8131bf741c10678a709f649a7cbb5b43f3d223
20210779@ricaldone.edu.sv <20210779@ricaldone.edu.sv>
2024-08-22 15:12:14 -0600

    Cambios realizados

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+// Hooks de React
+import { useEffect, useState } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+// Utilidades de React Navigation
 import { NavigationContainer } from '@react-navigation/native'; // Importa el contenedor de navegación
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; // Importa el creador de stack navigator
 
@@ -11,9 +15,65 @@ import DetalleProducto from './src/screens/DetalleProducto.js';
 import EditPersonalInfo from './src/screens/editrProfile.js';
 import Profile from './src/screens/Profile.js';
 
+//librerias para el splash screen
+
+import 'intl-pluralrules';
+
 export default function App() {
 
   const Stack = createNativeStackNavigator(); // Crea una instancia del stack navigator
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [logueado, setLogueado] = useState(false);
+
+   
+  // URL de la API para el usuario
+  const USER_API = "servicios/publico/cliente.php";
+
+  // Función que ayuda a verificar si existe previamente una sesión abierta
+  const verifyLogged = async () => {
+    try {
+      const data = await fetchData(USER_API, 'getUser');
+      if (data.session) {
+        console.log(data);
+        setLogueado(true);
+        //navigation.navigate("Home");
+      } else {
+        console.log(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function inicia() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 4000));
+        await verifyLogged();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    inicia();
+  }, []);
+
+  if (!appIsReady) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/HGLogo.gif')}
+          onLoadEnd={async () => {
+            await SplashScreen.hideAsync();
+          }}
+          style={styles.gif}
+        />
+      </View>
+    );
+  }
+
 
   return (
     <NavigationContainer> 
@@ -35,3 +95,16 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
+  gif: {
+    width: 200,
+    height: 200,
+  },
+});
--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -2,7 +2,7 @@
 //Ip de la maquina local 
 //utilizar la direccion IP del servidor y no localhost
 //export const IP = 'http://192.168.1.7';
-export const IP = 'http://10.10.2.163';
+export const IP = 'https://www.wilfredo.site/';
 //export const IP = 'http://192.168.29.75';
 export const SERVER_URL = `${IP}/HERMESPEED/api/`;
 export const IMAGES_URL = `${IP}/HERMESPEED/api/images/`;
